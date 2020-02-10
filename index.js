2d6850dde976dd0806537417a95ec47224037489
Shadow <taltaro2@gmail.com>
2020-02-10 11:35:03 -0500

    Start

---
+++ index.js
@@ -0,0 +1,17 @@
+let board = [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+];
+
+let playey1 = 'X';
+let playey2 = 'O';
+
+
+function setup() {
+    createCanvas(400,400);
+}
+
+function draw() {
+    background(220);
+}
